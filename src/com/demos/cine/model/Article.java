d74184bebfa60b3946a24286e7e8bf67785c4709
unknown <STAGIAIRE@R304-11.demostraining.com>
2016-05-12 12:19:25 +0200

    premier commit de la classe article

---
+++ src/com/demos/cine/model/Article.java
@@ -0,0 +1,62 @@
+package com.demos.cine.model;
+
+public class Article {
+	
+	private String reference;
+	private String designation;
+	private double prixHT;
+	private int  qteStock;
+	
+	
+	public Article() {
+	}
+
+
+	public Article(String reference, String designation, double prix, int qtestock) {
+		this.reference = reference;
+		this.designation = designation;
+		this.prixHT = prix;
+		this.qteStock = qtestock;
+	}
+
+
+	public double getPrix() {
+		return prixHT;
+	}
+
+
+	public void setPrix(double prix) {
+		this.prixHT = prix;
+	}
+
+
+	public String getReference() {
+		return reference;
+	}
+
+
+	public String getDesignation() {
+		return designation;
+	}
+
+
+	public int getQtestock() {
+		return qteStock;
+	}
+	
+	public void incrementStock(int val){
+		this.qteStock = this.qteStock+val;
+	}
+
+	public void decrementStock(int val){
+		this.qteStock = this.qteStock-val;
+	}
+
+
+	public String toString() {
+		return "Article [reference=" + reference + ", designation=" + designation + ", prixHT=" + prixHT + ", qteStock="
+				+ qteStock + "]";
+	}
+	
+	
+}
